5b1ffa5f836beb84a99388d19f2b94297995dc0a
Otavio Bernardinelli Ballico <otaviobernardinelli@gmail.com>
2023-06-06 23:45:24 -0300

    Calculadora

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById('calcular').addEventListener('click', function() {
+    var valorInicial = parseFloat(document.getElementById('valor-inicial').value);
+    var taxaJuros = parseFloat(document.getElementById('taxa-juros').value);
+    var valorMensal = parseFloat(document.getElementById('valor-mensal').value);
+    var periodo = parseInt(document.getElementById('periodo').value);
+  
+    var resultado
+}
+    );
+
+    window.addEventListener('scroll', function() {
+        var navbar = document.querySelector('.navbar');
+        if (window.scrollY > 0) {
+          navbar.classList.add('fixed-top');
+        } else {
+          navbar.classList.remove('fixed-top');
+        }
+      });
+      
